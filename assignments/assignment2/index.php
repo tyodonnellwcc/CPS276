f358786e42db8e4a0c4e4aa3556f9f4aa2ad99d1
Tyler O'Donnell <tyodonnell@wccnet.com>
2025-09-22 21:05:10 -0400

    updates

--- assignments/assignment2/index.php
+++ assignments/assignment2/index.php
@@ -1,16 +1,11 @@
 <?php
-    $numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50];
+    $numbers = range(1, 50);
 
-    $evenNumbers = foreach ($numbers as $number) {
-        if ($number == 1 || 3 || 5 || 7 || 9 || 11 || 13 || 15 || 17 || 19 || 21 || 23 || 25 || 27 || 29 || 31 || 33 || 35 || 37 || 39 || 41 || 43 || 45 || 47 || 49) {
-            continue;
+    $evenNumbers = "Even numbers: ";
+    foreach ($numbers as $number) {
+        if ($number % 2 !== 0) continue;
+            $evenNumbers .= ($number == 50) ? $number : "$number - ";
         }
-        if ($number == 50) {
-            echo $number;
-        } else {
-            echo "$number - ";
-        }
-    };
 
     $form = <<<EOD
         <div class="mb-3">
@@ -24,16 +19,17 @@
         EOD;
 
     function createTable($rows, $columns) {
-        $table = "";
-        for ($i = 1; $i <= 8; $i++) {
-            $table .= "";
-        for ($j = 1; $j <= 6; $j++) {
-            $table .= "";
+        $table = "<table class='table table-bordered'>";
+        for ($i = 1; $i <= $rows; $i++) {
+            $table .= "<tr>";
+        for ($j = 1; $j <= $columns; $j++) {
+            $table .= "<td>Row $i, Col $j</td>";
         }
-        $table .= "";
+        $table .= "</tr>";
     }
-    $table .= "Row $i, Col $j";
-    }
+    $table .= "</table>";
+    return $table;
+}
 ?>
 
 <!doctype html>
