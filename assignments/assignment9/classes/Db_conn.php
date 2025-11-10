8ea8ad4079928297e06b963955b4f4f07f3af8e5
Tyler O'Donnell <tyodonnell@wccnet.com>
2025-11-10 12:08:36 -0500

    Updates

---
+++ assignments/assignment9/classes/Db_conn.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sticky Form Example</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container mt-5">
+<p>&nbsp;</p>    
+<form method="post" action="">
+        <div class="row">
+            <!-- Render first name field -->
+            <div class="col-md-6">
+                <div class="mb-3">
+    <label for="first_name">First Name</label>
+    <input type="text" class="form-control" id="first_name" name="firstName" value="Scott">
+    
+</div>            </div>
+
+            <!-- Render last name field -->
+            <div class="col-md-6">
+                <div class="mb-3">
+    <label for="last_name">Last Name</label>
+    <input type="text" class="form-control" id="last_name" name="lastName" value="Shaper">
+    
+</div>            </div>
+        </div>
+
+        
+        <!-- Render email password password -->
+        <div class="row">
+           
+            <div class="col-md-4">
+                <div class="mb-3">
+    <label for="email">Email</label>
+    <input type="text" class="form-control" id="email" name="email" value="sshaper@wccnet.edu">
+    
+</div>            </div>
+            <div class="col-md-4">
+                <div class="mb-3">
+    <label for="password1">Password</label>
+    <input type="text" class="form-control" id="password1" name="password1" value="Pass$or1">
+    
+</div>            </div>
+            <div class="col-md-4">
+                <div class="mb-3">
+    <label for="password2">Confirm Password</label>
+    <input type="text" class="form-control" id="password2" name="password2" value="Pass$or1">
+    
+</div>            </div>
+        </div>
+     
+        <input type="submit" class="btn btn-primary" value="Register">
+    </form>
+        <table class="table table-bordered mt-2">
+        <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Password</th>
+        </tr><tr>
+            <td>Scott</td>
+            <td>Shaper</td>
+            <td>sshaper@wccnet.edu</td>
+            <td>$2y$10$qla5z5ydrGEeUWAf5MahFetNBzxA/i71qa2IMUvCg8pxpNVvRfoVq</td>
+        </tr><tr>
+            <td>Scott</td>
+            <td>Shaper</td>
+            <td>sshaper1@wccnet.edu</td>
+            <td>$2y$10$/YZgUxbIhWjjgs.NZfjW5OmwG6Uk21US8o7NtRujhVgsdiAVS/.BS</td>
+        </tr><tr>
+            <td>Chase</td>
+            <td>Miller</td>
+            <td>chaumiller@wccnet.edu</td>
+            <td>$2y$10$4Uuc348ZqzYEBYjDI6dOkO2MxadGR147M4ka3GXN9JwzwQkghJUhK</td>
+        </tr><tr>
+            <td>Scott</td>
+            <td>Shaper</td>
+            <td>bob@wccnet.edu</td>
+            <td>$2y$10$lp1bEBPG2kpwtTuVPuPoH.5Gbsh6SK1L0Uy.b4s6Ta8XIBqxK1Ww2</td>
+        </tr><tr>
+            <td>Scott</td>
+            <td>Shaper</td>
+            <td></td>
+            <td>$2y$10$4WZqOYjhCvU2.hlqQUeMTOoFQtzllMCF9j206UWEoEi9BOlVu8muS</td>
+        </tr><tr>
+            <td>Scott</td>
+            <td></td>
+            <td>chmiller@wccnet.edu</td>
+            <td>$2y$10$PKgE9dJYGd.D.VdZ.HPfGuGz1qPCC5X7KKmSG3iShEMe9zitodVEW</td>
+        </tr><tr>
+            <td>Sc</td>
+            <td>Shap</td>
+            <td>sshr@wccnet.edu</td>
+            <td>$2y$10$9KOrGVqCcWewpC0gouLFwuYeCiBpDKkfH3UJxXHYQLi4soqucB8hS</td>
+        </tr><tr>
+            <td>Holly</td>
+            <td>Price</td>
+            <td>holly@gmail.com</td>
+            <td>$2y$10$WNqAp6KQlLNsqbUj/GuGpuul7Io8nEMeaH5rfx4xew0.jPjbv0Gg.</td>
+        </tr><tr>
+            <td>test</td>
+            <td>test</td>
+            <td>test@gmail.com</td>
+            <td>$2y$10$VxrSA3DiVFAeLLG/XtMnxuurS6FwZM/NBEiLVx7BqcZp2wDXrFlva</td>
+        </tr><tr>
+            <td>Scott</td>
+            <td>Shaper</td>
+            <td>awfage@gmail.com</td>
+            <td>$2y$10$eLZ/vHTvc/g86t/qgzZkgePSC6.aavq0fTXeeu261qbeiG/zErCxK</td>
+        </tr><tr>
+            <td>read</td>
+            <td>comment</td>
+            <td>readcommet@wccnet.edu</td>
+            <td>$2y$10$gCCY5ImeYOswnkR3ZDZlAO1952vTsBgdtnUMntI7IJVue8k/d1rfO</td>
+        </tr><tr>
+            <td>Scott</td>
+            <td>Shaper</td>
+            <td>sshaper@wccnet.eedu</td>
+            <td>$2y$10$24T7gCwmTykD.b9joR2XseFc6d5azXzvX5m0eEx/MMv3E91Ckr0kq</td>
+        </tr><tr>
+            <td>Scott</td>
+            <td>Shaper</td>
+            <td>sshaper@wccnet.edud</td>
+            <td>$2y$10$GARAVknl9RgybUhRGuRxsutyJDTy6CwhHlyZaUHhm0kTP84CR/.zu</td>
+        </tr><tr>
+            <td>a</td>
+            <td></td>
+            <td>sshaper22@wccnet.edu</td>
+            <td>$2y$10$RwEqdtRrrN2jSKnJ6KlLEe.uD1hfFKSXTsQmwTCp3bf7koyXFoRBa</td>
+        </tr><tr>
+            <td>    </td>
+            <td>test</td>
+            <td>tes32@gmail.com</td>
+            <td>$2y$10$I/HxBB3DwLdMbwdvRXzEEO6oP4T47vCJZlLG7jAcgoYvBXl0F7FjK</td>
+        </tr><tr>
+            <td>Check</td>
+            <td>Testing</td>
+            <td>test@nothingmail.org</td>
+            <td>$2y$10$R92crA5kC4UduHFqz1QaLuCb0QUGNGIWZaH4ijHOotkvZwhBjNYf6</td>
+        </tr><tr>
+            <td>m</td>
+            <td></td>
+            <td>test@nothin3gmail.org</td>
+            <td>$2y$10$GXuEJ15EEPm7koGLtXluCufp2QOukWD1ls8FoCEoU/7KDw5Ae/XDe</td>
+        </tr></table></div>
+
+</body>
+</html>
